cad7b35c4ed130933e0fa6552e38b3cf7d90fdc0
bimapw4 <bimapw4@gmail.com>
2022-11-27 12:12:24 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Products struct {
+	ID         int
+	Name       string
+	Pricelists Pricelists
+}
+
+type Pricelists struct {
+	ID       int
+	Category string
+	Brand    string
+	Price    int
+}
+
+var jsonSample = `
+[
+  {
+    "ID": 1,
+    "Name": "Pena Hitam",
+    "Price": 0,
+    "Pricelists": {
+      "ID": 1,
+      "Category": "pena",
+      "Brand": "bagus"
+    }
+  },
+  {
+    "ID": 2,
+    "Name": "Pena Hitam",
+    "Price": 0,
+    "Pricelists": {
+      "ID": 1,
+      "Category": "pena",
+      "Brand": "laris"
+    }
+  }
+]
+`
+
+func main() {
+	mapArr := getMaps()
+	if bte, e := json.Marshal(mapArr); nil == e {
+		fmt.Println(string(bte))
+	}
+}
+
+func getMaps() map[string]map[string]string {
+	var products []Products
+	output := map[string]map[string]string{}
+	if e := json.Unmarshal([]byte(jsonSample), &products); nil != e {
+		panic(e)
+	}
+
+	for i := range products {
+		productToMapItem(products[i], output)
+	}
+
+	return output
+}
+
+func productToMapItem(product Products, output map[string]map[string]string) {
+	// ... ISI JAWABAN KAMU PADA FUNGSI INI
+}
